8bacc9a0e40e6c7d41ba2113d606b99650b673fd
Richie <alfaruaris87@gmail.com>
2025-06-07 09:13:59 -0700

    Add existing file

---
+++ script.js
@@ -0,0 +1,3 @@
+function showForm(formId) {
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
